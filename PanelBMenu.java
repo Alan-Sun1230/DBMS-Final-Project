74ee0f769e7440fd177b000e898ad5820857538d
Alan-Sun1230 <117022253+Alan-Sun1230@users.noreply.github.com>
2024-06-06 16:39:17 +0800

    6/6 16:39

--- PanelBMenu.java
+++ PanelBMenu.java
@@ -25,23 +25,27 @@ public class PanelBMenu extends JPanel{
             JPanel pTemp = new JPanel();
             pTemp.setLayout(new BoxLayout(pTemp, BoxLayout.Y_AXIS));
             pTemp.setPreferredSize(new Dimension(700,200));
-
+            
+            JLabel lblMenuID = new JLabel(Integer.toString(listMenu.get(i).getmenuID()));
             JLabel lblItem = new JLabel(listMenu.get(i).getItem());
             JLabel lblPrice = new JLabel(Integer.toString(listMenu.get(i).getPrice()));
             JLabel lblCost = new JLabel(Integer.toString(listMenu.get(i).getCost()));
-
-            JLabel lblText1 = new JLabel("Name: ");
-            JLabel lblText2 = new JLabel("Price: ");
-            JLabel lblText3 = new JLabel("Cost: ");
-
+            
+            JLabel lblText1 = new JLabel("MenuID: ");
+            JLabel lblText2 = new JLabel("Item: ");
+            JLabel lblText3 = new JLabel("Price: ");
+            JLabel lblText4 = new JLabel("Cost: ");
+            
             JTextField tf_Item = new JTextField(10);
             JTextField tf_Price = new JTextField(10);
             JTextField tf_Cost = new JTextField(10);
-
+            
+            JPanel pMenuID = new JPanel(new FlowLayout(FlowLayout.LEFT,5,0));
             JPanel pItem = new JPanel(new FlowLayout(FlowLayout.LEFT,5,0));
             JPanel pPrice = new JPanel(new FlowLayout(FlowLayout.LEFT,5,0));
             JPanel pCost = new JPanel(new FlowLayout(FlowLayout.LEFT,5,0));
-
+            
+            pMenuID.add(lblText1);pMenuID.add(lblMenuID);
             pItem.add(lblText1);pItem.add(lblItem);pItem.add(tf_Item);
             pPrice.add(lblText2);pPrice.add(lblPrice);pPrice.add(tf_Price);
             pCost.add(lblText3);pCost.add(lblCost);pCost.add(tf_Cost);
@@ -49,30 +53,38 @@ public class PanelBMenu extends JPanel{
             JButton bt_Edit = new JButton("Edit");
             bt_Edit.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent event) {
-                    if (!tf_Item.getText().isEmpty()) {
-                        String updatedItem = tf_Item.getText();
-                        lblItem.setText(updatedItem);
-                    } else {
-                        String updatedItem = lblItem.getText();
-                        lblItem.setText(updatedItem);
-                    }
+                	try {
+                		if (!tf_Item.getText().isEmpty()) {
+                            String updatedItem = tf_Item.getText();
+                            lblItem.setText(updatedItem);
+                            Bar.getInstance().UpdateMenuDB(Integer.parseInt(lblMenuID.getText()), "Item", updatedItem);
+                        } else {
+                            String updatedItem = lblItem.getText();
+                            lblItem.setText(updatedItem);
+                        }
 
-                    if (!tf_Price.getText().isEmpty()) {
-                        String updatedPrice = tf_Price.getText();
-                        lblPrice.setText(updatedPrice);
-                    } else {
-                        String updatedPrice = lblPrice.getText();
-                        lblPrice.setText(updatedPrice);
-                    }
+                        if (!tf_Price.getText().isEmpty()) {
+                            String updatedPrice = tf_Price.getText();
+                            lblPrice.setText(updatedPrice);
+                            Bar.getInstance().UpdateMenuDB(Integer.parseInt(lblMenuID.getText()), "Price", updatedPrice);
+                        } else {
+                            String updatedPrice = lblPrice.getText();
+                            lblPrice.setText(updatedPrice);
+                        }
 
-                    if (!tf_Cost.getText().isEmpty()) {
-                        String updatedCost = tf_Cost.getText();
-                        lblCost.setText(updatedCost);
-                    } else {
-                        String updatedCost = lblCost.getText();
-                        lblCost.setText(updatedCost);
-                    }
-
+                        if (!tf_Cost.getText().isEmpty()) {
+                            String updatedCost = tf_Cost.getText();
+                            lblCost.setText(updatedCost);
+                            Bar.getInstance().UpdateMenuDB(Integer.parseInt(lblMenuID.getText()), "Cost", updatedCost);
+                        } else {
+                            String updatedCost = lblCost.getText();
+                            lblCost.setText(updatedCost);
+                        }
+                	}catch (WrongDataError e) {
+                		e.printStackTrace();
+                	}
+                	
+                    
                     //TODO: update DB with three Strings
                 }
             });
@@ -103,12 +115,11 @@ public class PanelBMenu extends JPanel{
     private void buildMenuList() {
         if (build) {
             //TODO: build from database
-            listMenu.add(new MenuItemObject("test1", 100, 10));
-            listMenu.add(new MenuItemObject("test2", 200, 20));
-            listMenu.add(new MenuItemObject("test3", 300, 30));
-            listMenu.add(new MenuItemObject("test4", 400, 40));
-            listMenu.add(new MenuItemObject("test5", 500, 50));
-            listMenu.add(new MenuItemObject("test6", 600, 60));
+        	try {
+        		Bar.getInstance().buildListMenu(listMenu);
+        	}catch (WrongDataError e) {
+        		e.printStackTrace();
+        	}
         }
         
         build = false;
