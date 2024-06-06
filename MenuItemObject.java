ea3b380469a69fbd9eaa9c3a7d9705bc958b5a3f
Alan-Sun1230 <117022253+Alan-Sun1230@users.noreply.github.com>
2024-06-06 16:31:34 +0800

    6/6 16:31

--- MenuItemObject.java
+++ MenuItemObject.java
@@ -1,12 +1,18 @@
 public class MenuItemObject {
+	private int menuID;
     private String item;
     private int price;
     private int cost;
 
-    public MenuItemObject(String item,int price,int cost) {
+    public MenuItemObject(int menuID, String item,int price,int cost) {
+    	this.menuID = menuID;
         this.item = item;
         this.price = price;
         this.cost = cost;
+    }
+    
+    public int getmenuID() {
+    	return menuID;
     }
 
     public String getItem() {
@@ -16,6 +22,11 @@ public class MenuItemObject {
     public int getPrice() {
         return price;
     }
+
+    public int getCost() {
+        return cost;
+    }
+}
 
     public int getCost() {
         return cost;
